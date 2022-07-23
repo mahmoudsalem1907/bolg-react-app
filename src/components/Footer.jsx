0f7677af572f50370b1171e07c9475eaded4f344
mahmoudsalem1907 <62968091+mahmoudsalem1907@users.noreply.github.com>
2022-07-23 05:07:45 +0200

    blog-app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-700 p-5">
+      <div className="container m-auto">
+        <p className="text-center text-white">
+          All Copyrights Reserved &copy; 2022 For Mahmoud Trabai
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
